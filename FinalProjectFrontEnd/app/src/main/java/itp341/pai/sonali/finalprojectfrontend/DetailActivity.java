3561c0f9a352fd68c7c985f116f44dab0a795f9a
Eric Su <esu@usc.edu>
2017-11-16 12:29:23 -0800

    front end

---
+++ FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/DetailActivity.java
@@ -0,0 +1,157 @@
+package itp341.pai.sonali.finalprojectfrontend;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import itp341.pai.sonali.finalprojectfrontend.model.Comment;
+import itp341.pai.sonali.finalprojectfrontend.model.GET_HTTP;
+import itp341.pai.sonali.finalprojectfrontend.model.Toilet;
+
+/**
+ * Created by Sonali Pai on 11/10/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    private int bathroomId = -1;
+    private TextView bathroomNameView;
+    private TextView bathroomDescView;
+    private TextView addressView;
+    private ListView commentsView;
+    private Toilet bathroom;
+    private ImageView disabledIcon;
+    private ImageView keyIcon;
+    FloatingActionButton fabButton;
+    FloatingActionButton fabImage;
+    FloatingActionButton fabComment;
+    private ArrayAdapter<String> adap;
+    private boolean isFABOpen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_toilet);
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#000000"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
+        Window window = this.getWindow();
+
+        //change color of status bar
+        window.setStatusBarColor(getResources().getColor(android.R.color.black));
+
+
+        Intent i = getIntent();
+        final Toilet t = (Toilet)i.getSerializableExtra("toilet");
+//        bathroomId = getIntent().getIntExtra("bathroomId", -1);
+//
+//        try {
+//            URL url_getBathroom = new URL("http://localhost:8080/BathroomServlet?bathroomId=" + bathroomId);
+//            GET_HTTP getBathroomHTTP = new GET_HTTP(url_getBathroom);
+//            String bathroomJson = getBathroomHTTP.getResponse();
+//            Gson gson = new Gson();
+//            bathroom = gson.fromJson(bathroomJson, Toilet.class);
+//        } catch (MalformedURLException mue) {
+//            mue.getStackTrace();
+//        } catch (IOException ioe) {
+//            ioe.getStackTrace();
+//        }
+        commentsView = (ListView) findViewById(R.id.comments);
+        bathroomNameView = (TextView) findViewById(R.id.toilet_name);
+        bathroomNameView.setText(t.getNameOfLocation());
+       // bathroomDescView = (TextView) findViewById(R.id.toilet_description);
+       // bathroomNameView.setText(bathroom.getDescription());
+        commentsView = (ListView) findViewById(R.id.comments);
+        List<String> comments = t.getComments();
+        disabledIcon = (ImageView) findViewById(R.id.disabledCheck);
+        keyIcon = (ImageView) findViewById(R.id.keyCheck);
+//        ArrayAdapter<Comment> commentsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, comments);
+//        commentsView.setAdapter(commentsAdapter);
+        addressView = (TextView) findViewById(R.id.address);
+        addressView.setText(t.getAddress());
+        t.addComments("This bathroom is pretty clean");
+        t.addComments("One of my favorite locations.");
+        t.addComments("Good.");
+        t.addComments("This is a comment");
+        t.addComments("Satisfactory.");
+        adap = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,t.getComments());
+        commentsView.setAdapter(adap);
+        if(t.isHasDisabilityAccomodations()){
+            disabledIcon.setImageResource(R.drawable.ic_done_black_24px);
+            disabledIcon.setColorFilter(Color.rgb(50,205,50));
+        }else{
+            disabledIcon.setImageResource(R.drawable.ic_close_black_24px);
+            disabledIcon.setColorFilter(Color.RED);
+        }
+        if(t.isRequiresKey()){
+            keyIcon.setImageResource(R.drawable.ic_done_black_24px);
+            keyIcon.setColorFilter(Color.rgb(50,205,50));
+        }else{
+            keyIcon.setImageResource(R.drawable.ic_close_black_24px);
+            keyIcon.setColorFilter(Color.RED);
+        }
+
+        fabButton = (FloatingActionButton) findViewById(R.id.fab);
+        fabImage = (FloatingActionButton) findViewById(R.id.fabImage);
+        fabComment = (FloatingActionButton) findViewById(R.id.fabComment);
+
+        //floating action button listener
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    isFABOpen=true;
+                    float deg = fabButton.getRotation() + 45F;
+                    fabButton.animate().rotation(deg).setInterpolator(new AccelerateDecelerateInterpolator());
+                    fabImage.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+                    fabComment.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+                }else{
+                    float deg = fabButton.getRotation() + 45F;
+                    fabButton.animate().rotation(deg).setInterpolator(new AccelerateDecelerateInterpolator());
+                    isFABOpen=false;
+                    fabImage.animate().translationY(0);
+                    fabComment.animate().translationY(0);
+                }
+            }
+        });
+        fabComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),CommentActivity.class);
+                i.putExtra("toilet",t);
+                startActivityForResult(i, 0);
+            }
+        });
+        fabImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 0);
+
+            }
+        });
+
+    }
+
+}
---
+++ FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/model/Toilet.java
@@ -0,0 +1,116 @@
+package itp341.pai.sonali.finalprojectfrontend.model;
+
+import android.media.Image;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sonali Pai on 11/8/2017.
+ */
+
+public class Toilet implements Serializable{
+
+    //data members
+    private long bathroomId;
+    private String name;
+    private String address;
+    private boolean hasDisabilityAccomodations;
+    private boolean requiresKey;
+    private int points;
+
+    private boolean upArrowSelected;
+    private boolean downArrowSelected;
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void addComments(String comment) {
+       comments.add(comment);
+    }
+
+    private List<String> comments;
+    public Toilet(String nameOfLocation, String address, boolean hadDisabilityAccomodations, boolean requiresKey) {
+        this.name = nameOfLocation;
+        this.address = address;
+        this.hasDisabilityAccomodations = hadDisabilityAccomodations;
+        this.requiresKey = requiresKey;
+        points = 0;
+        upArrowSelected=false;
+        downArrowSelected=false;
+        comments = new ArrayList<String>();
+    }
+
+    public boolean isUpArrowSelected() {
+        return upArrowSelected;
+    }
+
+    public void setUpArrowSelected(boolean upArrowSelected) {
+        this.upArrowSelected = upArrowSelected;
+    }
+
+    public boolean isDownArrowSelected() {
+        return downArrowSelected;
+    }
+
+    public void setDownArrowSelected(boolean downArrowSelected) {
+        this.downArrowSelected = downArrowSelected;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+
+    public boolean isHasDisabilityAccomodations() {
+        return hasDisabilityAccomodations;
+    }
+
+    public void setHasDisabilityAccomodations(boolean hasDisabilityAccomodations) {
+        this.hasDisabilityAccomodations = hasDisabilityAccomodations;
+    }
+
+    public boolean isRequiresKey() {
+        return requiresKey;
+    }
+
+    public void setRequiresKey(boolean requiresKey) {
+        this.requiresKey = requiresKey;
+    }
+
+    public long getBathroomId() {
+        return bathroomId;
+    }
+
+    public void setBathroomId(long bathroomId) {
+        this.bathroomId = bathroomId;
+    }
+
+    public String getNameOfLocation() {
+        return name;
+    }
+
+    public boolean getDisabilityAccomodations() {
+        return hasDisabilityAccomodations;
+    }
+
+    public void setNameOfLocation(String nameOfLocation) {
+        this.name = nameOfLocation;
+    }
+
+    public void setDisabilityAccomodations(boolean disabilityAccomodations) {
+        this.hasDisabilityAccomodations = disabilityAccomodations;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public String getAddress(){
+        return address;
+    }
+}
