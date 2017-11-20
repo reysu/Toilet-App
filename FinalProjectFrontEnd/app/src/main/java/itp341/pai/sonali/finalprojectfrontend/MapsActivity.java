8350d0ac32547fd572232ea5000805d49ddce49c
Chuanxiu Xiong (Ethan) <chuanxix@usc.edu>
2017-11-19 21:01:40 -0800

    Update MapsActivity.java
    zoomed-in-map

--- FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/MapsActivity.java
+++ FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/MapsActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
