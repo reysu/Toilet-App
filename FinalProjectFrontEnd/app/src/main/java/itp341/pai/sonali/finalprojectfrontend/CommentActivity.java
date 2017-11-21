63fa2779aec82890c1bb5406ac9a68d14d23cd36
Eric Su <esu@usc.edu>
2017-11-20 23:04:04 -0800

    comments

--- FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/CommentActivity.java
+++ FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/CommentActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Message;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,8 +16,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
@@ -27,6 +30,7 @@ import java.io.IOException;
 
 import itp341.pai.sonali.finalprojectfrontend.AddToiletActivity;
 import itp341.pai.sonali.finalprojectfrontend.R;
+import itp341.pai.sonali.finalprojectfrontend.model.Comment;
 import itp341.pai.sonali.finalprojectfrontend.model.Toilet;
 import itp341.pai.sonali.finalprojectfrontend.model.User;
 
@@ -58,36 +62,39 @@ public class CommentActivity extends AppCompatActivity{
                 t.addComments(commentText);
 
                 OkHttpClient client = new OkHttpClient();
-                int userId = Integer.parseInt(MainActivity.USERID); //the current user adding comment
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                int userId = prefs.getInt("USERID", -1);; //the current user adding comment
+                Comment newComment = new Comment("", commentText,userId, (int) t.getBathroomId());
+                String json = new GsonBuilder().create().toJson(newComment, Comment.class);
+                try {
+                    RequestBody body =  RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
 
-                RequestBody formBody = new FormEncodingBuilder().add("bathroomid", Long.toString(t.getBathroomId())).build();
-                Request request = new Request.Builder()
-                        .url("http://ec2-54-86-4-0.compute-1.amazonaws.com:8080/bathroom")
-                        .post(formBody)
-                        .build();
+                    Request request = new Request.Builder()
+                            .url("http://ec2-54-86-4-0.compute-1.amazonaws.com:8080/bathroom/"+t.getBathroomId()+"/comment")
+                            .post(body)
+                            .build();
 
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Request request, IOException e) {
-                        e.printStackTrace();
-                    }
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Request request, IOException e) {
+                            e.printStackTrace();
+                        }
 
-                    @Override
-                    public void onResponse(Response response) throws IOException {
-                        try (ResponseBody responseBody = response.body()) {
-                            if(!response.isSuccessful()) {
-                            }else {
-                                String userJsonValidate = response.body().string();
-
-                                Intent i = new Intent(getApplicationContext(), ListActivity.class);
-                                i.putExtra("guest", false);
-                                startActivity(i);
+                        @Override
+                        public void onResponse(Response response) throws IOException {
+                            try (ResponseBody responseBody = response.body()) {
+                                if (!response.isSuccessful()) {
+                                } else {
+                                    setResult(Activity.RESULT_OK);
+                                    finish();
+                                }
                             }
                         }
-                    }
-                });
-                setResult(Activity.RESULT_OK);
-                finish();
+                    });
+                } catch (Exception ie){
+
+                }
+
 
             }
         });
