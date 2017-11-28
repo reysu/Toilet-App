b99ddc165b5ae701a3736b81ab7bd4cad78c1ac3
Chuanxiu Xiong (Ethan) <chuanxix@usc.edu>
2017-11-28 02:04:53 -0800

    Update Gallery.java

--- FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/Gallery.java
+++ FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/Gallery.java
@@ -35,8 +35,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import itp341.pai.sonali.finalprojectfrontend.model.Photo;
+import itp341.pai.sonali.finalprojectfrontend.model.SquaredImageView;
 import itp341.pai.sonali.finalprojectfrontend.model.Toilet;
 import itp341.pai.sonali.finalprojectfrontend.model.User;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import com.squareup.picasso.Picasso;
+import static android.widget.ImageView.ScaleType.CENTER_CROP;
 
 /**
  * Created by reysu on 11/17/17.
@@ -47,13 +54,11 @@ public class Gallery extends AppCompatActivity {
     private Toilet t = null;
     private List<Photo> photos;
     private List<String> imgUrls = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_gallery);
-
-        GridView gridview = (GridView) findViewById(R.id.gallery);
-        gridview.setAdapter(new ImageAdapter(this));
         Intent i = getIntent();
         t = (Toilet) i.getSerializableExtra("toilet");
 
@@ -70,20 +75,32 @@ public class Gallery extends AppCompatActivity {
             @Override
             public void onResponse(Response response) throws IOException {
                 try (ResponseBody responseBody = response.body()) {
-                    if (!response.isSuccessful()) {
+                    if (response.isSuccessful()) {
                         String photoJson = response.body().string();
                         Gson gson = new Gson();
                         Type targetClassType = new TypeToken<ArrayList<Photo>>(){}.getType();
                         List<Photo> photostemp = gson.fromJson(photoJson,targetClassType);
                         for(int j=0;j<photostemp.size();j++){
-                            photos.add(photostemp.get(j));
+                            imgUrls.add("http://ec2-54-86-4-0.compute-1.amazonaws.com:8080/" + photostemp.get(j).getUrl());
+
                         }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                GridView gridview = (GridView) findViewById(R.id.gallery);
+                                gridview.setAdapter(new ImageAdapter(Gallery.this));
+
+                            }
+                        });
                     } else {
 
                     }
                 }
             }
         });
+
+
 
     }
 
@@ -96,62 +113,86 @@ public class Gallery extends AppCompatActivity {
         }
 
         public int getCount() {
-            return 0;//mThumbIds.length;
+            return imgUrls.size();
         }
 
         public Object getItem(int position) {
-            return null;
+            return imgUrls.get(position);
         }
 
         public long getItemId(int position) {
-            return 0;
+            return position;
         }
 
         // create a new ImageView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            if (convertView == null) {
-                // if it's not recycled, initialize some attributes
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(8, 8, 8, 8);
-            } else {
-                imageView = (ImageView) convertView;
+
+            SquaredImageView view = (SquaredImageView) convertView;
+            if (view == null) {
+                view = new SquaredImageView(mContext);
+                view.setScaleType(CENTER_CROP);
             }
 
-            new DownloadImageTask(imageView).execute(imgUrls.get(imageCounter));
-            ++imageCounter;
-            //   imageView.setImageResource(mThumbIds[position]);
-            return imageView;
+            // Get the image URL for the current position.
+            String url = (String)getItem(position);
+            // Trigger the download of the URL asynchronously into the image view.
+            Picasso.with(mContext) //
+                    .load(url) //
+                    .fit() //
+                    .into(view);
+
+            return view;
+
+//            ImageView imageView;
+//            if (convertView == null) {
+//                // if it's not recycled, initialize some attributes
+//                imageView = new ImageView(mContext);
+//                imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+//                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//                imageView.setPadding(8, 8, 8, 8);
+//            } else {
+//                imageView = (ImageView) convertView;
+//            }
+//            if (imageCounter < imgUrls.size())
+//            {
+//                new DownloadImageTask(imageView).execute("https://www.blog.google/static/blog/images/google-200x200.7714256da16f.png");//);
+//               // System.out.println("ImageURL::::::::::: " + imgUrls.get(imageCounter))   imgUrls.get(imageCounter);
+//                //imageView.setImageResource(imgUrls.get(imageCounter));
+//                ++imageCounter;
+//            }
+//            return imageView;
+         }
         }
         // references to our images
         // private String[] mThumbIds
-    }
 
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
+//
+//    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+//        ImageView bmImage;
+//
+//        public DownloadImageTask(ImageView bmImage) {
+//            this.bmImage = bmImage;
+//        }
+//
+//        protected Bitmap doInBackground(String... urls) {
+//            String urldisplay = urls[0];
+//            Bitmap mIcon11 = null;
+//            try {
+//                InputStream in = new java.net.URL(urldisplay).openStream();
+//                mIcon11 = BitmapFactory.decodeStream(in);
+//            } catch (Exception e) {
+//                Log.e("Error", e.getMessage());
+//                e.printStackTrace();
+//            }
+//
+//            return mIcon11;
+//        }
+//
+//        protected void onPostExecute(Bitmap result) {
+//            bmImage.setImageBitmap(result);
+//
+//
+//        }
+//    }
 }
